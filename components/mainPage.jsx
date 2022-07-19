ce0057ead1ae6ddebc5855ce9e59e62a1e4dcefb
brandonseverin <brandon.severin@materials.ox.ac.uk>
2022-07-19 09:36:33 +0100

    fix center bug

--- components/mainPage.jsx
+++ components/mainPage.jsx
@@ -1,284 +1,289 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-import {useState} from 'react'
-const snarkjs = require('snarkjs');
+import React from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { useState } from "react";
+const snarkjs = require("snarkjs");
 import { connect, getStarknet } from "get-starknet";
-import Link from 'next/link'
-import MapComponent from './maptest.jsx';
-import Router from 'next/router'
-
+import Link from "next/link";
+import MapComponent from "./maptest.jsx";
+import Router from "next/router";
 
 const containerStyle = {
-  width: '1100px',
-  height: '900px'
+  width: "1100px",
+  height: "900px",
 };
 
-var center = {
+export var center = {
   lat: 48.85,
-  lng: 2.35
+  lng: 2.35,
 };
 
-var position ={
-    lat:null,
-    lng:null
-}
+var position = {
+  lat: null,
+  lng: null,
+};
 
 function MyComponent() {
-
   const [isConnected, setIsConnected] = useState();
 
-
-  const [minLat,setMinLat] = useState(0);
-  const [maxLat,setMaxLat] = useState(150);
-  const [minLong,setMinLong] = useState(0);
-  const [maxLong,setMaxLong] = useState(150);
-  const [currentLat,setCurrentLat] = useState(120);  
-  const [currentLong,setCurrentLong] = useState(140);  
-  const [mainProof ,setMainProof] = useState();
-  const [mainPublicSignals,setMainPublicSignals] = useState();
-  const [isVerified,setIsVerified] = useState(false);
-
+  const [minLat, setMinLat] = useState(0);
+  const [maxLat, setMaxLat] = useState(150);
+  const [minLong, setMinLong] = useState(0);
+  const [maxLong, setMaxLong] = useState(150);
+  const [currentLat, setCurrentLat] = useState(120);
+  const [currentLong, setCurrentLong] = useState(140);
+  const [mainProof, setMainProof] = useState();
+  const [mainPublicSignals, setMainPublicSignals] = useState();
+  const [isVerified, setIsVerified] = useState(false);
 
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyAIJx9hwjxcmt2-VjyW5MCxwPqlVvTW51w"
-  })
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAIJx9hwjxcmt2-VjyW5MCxwPqlVvTW51w",
+  });
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    setMap(null);
+  }, []);
 
-  function sendProps(){
+  function sendProps() {
     Router.push({
-        pathname:"/select-nft",
-        query:{
-            minLat,
-            maxLat,
-            minLong,
-            maxLong,
-            currentLat,
-            currentLong
-        }
-
-    })
+      pathname: "/select-nft",
+      query: {
+        minLat,
+        maxLat,
+        minLong,
+        maxLong,
+        currentLat,
+        currentLong,
+      },
+    });
   }
-
-
 
   async function connectWallet() {
-      try {
-          const wallet = await connect({
-              include: ["braavos"],
-          });
-          if (wallet) {
-              await wallet.enable({ showModal: true });
-              setIsConnected(!!wallet?.isConnected);
-          }
-          const wallet2 = getStarknet();
-
-          const address = wallet2.account.address;
-          console.log(address);
-      } catch {
-          console.log("Connect Wallet")
+    try {
+      const wallet = await connect({
+        include: ["braavos"],
+      });
+      if (wallet) {
+        await wallet.enable({ showModal: true });
+        setIsConnected(!!wallet?.isConnected);
       }
-  }
-  
-  
-  
+      const wallet2 = getStarknet();
 
-  
-    
-    
+      const address = wallet2.account.address;
+      console.log(address);
+    } catch {
+      console.log("Connect Wallet");
+    }
+  }
+
   async function getFileBuffer(filename) {
-      let req = await fetch(filename);
-      return Buffer.from(await req.arrayBuffer());
+    let req = await fetch(filename);
+    return Buffer.from(await req.arrayBuffer());
   }
 
   async function generateProof(state, setState) {
+    // Load files and run proof locally
+    let DOMAIN = "http://localhost:3000";
+    let wasmBuff = await getFileBuffer(`${DOMAIN}/inRange.wasm`);
+    let zkeyBuff = await getFileBuffer(`${DOMAIN}/inRange.zkey`);
 
-      // Load files and run proof locally
-      let DOMAIN = "http://localhost:3000";
-      let wasmBuff = await getFileBuffer(`${DOMAIN}/inRange.wasm`);
-      let zkeyBuff = await getFileBuffer(`${DOMAIN}/inRange.zkey`);
-    
-      let input = {
-          "latitudeRange": [ minLat, maxLat],
-          "longitudeRange": [ minLong, maxLong],
-          "currentLocation": [ currentLat, currentLong]
-      }
-      console.log(input)
-    
-      try {
-    
-        let {proof, publicSignals} = await snarkjs.plonk.fullProve(input, wasmBuff, zkeyBuff);
-        console.log("Generated Proof")
-        console.log(proof)
-        setMainProof(proof);
-        setMainPublicSignals(publicSignals);
-    
+    let input = {
+      latitudeRange: [minLat, maxLat],
+      longitudeRange: [minLong, maxLong],
+      currentLocation: [currentLat, currentLong],
+    };
+    console.log(input);
+
+    try {
+      let { proof, publicSignals } = await snarkjs.plonk.fullProve(
+        input,
+        wasmBuff,
+        zkeyBuff
+      );
+      console.log("Generated Proof");
+      console.log(proof);
+      setMainProof(proof);
+      setMainPublicSignals(publicSignals);
+
       //   setState({...state, proof:proof, publicSignals:publicSignals})
-    
-      } catch (error) {
-        alert("Proof generation Failed: " + error)
-      } 
+    } catch (error) {
+      alert("Proof generation Failed: " + error);
     }
+  }
 
+  async function verifyProof(state, setState) {
+    let vkey = await fetch(
+      `http://localhost:3000/inRange_verification_key.json`
+    ).then((res) => res.json());
 
-  async function verifyProof(state , setState ) {
+    const verified = await snarkjs.plonk.verify(
+      vkey,
+      mainPublicSignals,
+      mainProof
+    );
 
-      let vkey = await fetch (`http://localhost:3000/inRange_verification_key.json`).then(res => res.json());
-    
-      const verified = await snarkjs.plonk.verify(vkey, mainPublicSignals, mainProof);
-      
-      console.log(`Proof Verification Result :${verified}`)
-      if(verified){
-        setIsVerified(true)
-      }
-    
+    console.log(`Proof Verification Result :${verified}`);
+    if (verified) {
+      setIsVerified(true);
     }
-    
+  }
 
+  const [location, setLocation] = useState({
+    lat: "",
+    long: "",
+  });
 
-  const [location,setLocation] = useState({
-      lat:"",
-      long:"",
-  })
+  function getLocation() {
+    const onSuccess = (loc) => {
+      setLocation({
+        lat: loc.coords.latitude,
+        long: loc.coords.longitude,
+      });
+      console.log("Locations");
+      //   console.log(location)
+      console.log(loc);
+      const lat = parseInt(loc.coords.latitude, 10);
+      const long = parseInt(loc.coords.longitude, 10);
+      setCurrentLat(lat);
+      setCurrentLong(long);
+    };
 
-  function getLocation(){
-      const onSuccess = (loc) => {
-          setLocation({
-              lat: loc.coords.latitude,
-              long: loc.coords.longitude
-          })
-          console.log("Locations")
-        //   console.log(location)
-          console.log(loc)
-          const lat = parseInt(loc.coords.latitude, 10);
-          const long = parseInt(loc.coords.longitude, 10);
-          setCurrentLat(lat)
-          setCurrentLong(long)
+    const onError = () => {
+      console.log("Could not get Location");
+    };
 
-      }
-
-      const onError =()=>{
-          console.log("Could not get Location")
-      }
-
-      navigator.geolocation.getCurrentPosition(onSuccess,onError)
- 
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
   }
 
   return (
-  <div className="flex justify-evenly mt-5">
-
-    {isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={4}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-        onClick={ev => {
+    <div className="flex justify-evenly mt-5">
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={4}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+          onClick={(ev) => {
             console.log("latitide = ", ev.latLng.lat());
             console.log("longitude = ", ev.latLng.lng());
-            
           }}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <><Marker
-      position={position}
-    /></>
-      </GoogleMap>
-  ) : <></>}
-  <div className="ml-2 mr-2">
-      <h3 className="text-lg font-medium text-gray-900 text-center mt-5">Latitude Range</h3>
-      <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2">
-       
-          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
-            <dt className="text-sm font-medium text-gray-500 truncate text-center">Min Latitude</dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">{minLat}</dd>
-          </div>
-          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
-            <dt className="text-sm font-medium text-gray-500 truncate text-center">Max Latitude</dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">{maxLat}</dd>
-          </div>
-   
-      </dl>
-      <h3 className="text-lg leading-6 font-medium text-gray-900 text-center mt-5">Longitude Range</h3>
-      <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2">
-      
-          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
-            <dt className="text-sm font-medium text-gray-500 truncate text-center">Min Longitude</dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">{minLong}</dd>
-          </div>
-          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
-            <dt className="text-sm font-medium text-gray-500 truncate text-center">Max Longitude</dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">{maxLong}</dd>
-          </div>
-     
-      </dl>
-      <h3 className="text-lg leading-6 font-medium text-gray-900 text-center mt-5">Current Location</h3>
-      <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2">
-     
-          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
-            <dt className="text-sm font-medium text-gray-500 truncate text-center">Current Latitude</dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">{currentLat}</dd>
-          </div>
-          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
-            <dt className="text-sm font-medium text-gray-500 truncate text-center">Current Latitude</dt>
-            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">{currentLong}</dd>
-          </div>
-      
-      </dl>
-      <button
-            type="button"
-            className="w-full mt-5 items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            onClick={getLocation}
         >
-        Get Location
-      </button>
-      <div className="mt-5 grid grid-cols-2 gap-1 sm:grid-cols-2">
+          {/* Child components, such as markers, info windows, etc. */}
+          <>
+            <Marker position={position} />
+          </>
+        </GoogleMap>
+      ) : (
+        <></>
+      )}
+      <div className="ml-2 mr-2">
+        <h3 className="text-lg font-medium text-gray-900 text-center mt-5">
+          Latitude Range
+        </h3>
+        <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2">
+          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
+            <dt className="text-sm font-medium text-gray-500 truncate text-center">
+              Min Latitude
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">
+              {minLat}
+            </dd>
+          </div>
+          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
+            <dt className="text-sm font-medium text-gray-500 truncate text-center">
+              Max Latitude
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">
+              {maxLat}
+            </dd>
+          </div>
+        </dl>
+        <h3 className="text-lg leading-6 font-medium text-gray-900 text-center mt-5">
+          Longitude Range
+        </h3>
+        <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2">
+          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
+            <dt className="text-sm font-medium text-gray-500 truncate text-center">
+              Min Longitude
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">
+              {minLong}
+            </dd>
+          </div>
+          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
+            <dt className="text-sm font-medium text-gray-500 truncate text-center">
+              Max Longitude
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">
+              {maxLong}
+            </dd>
+          </div>
+        </dl>
+        <h3 className="text-lg leading-6 font-medium text-gray-900 text-center mt-5">
+          Current Location
+        </h3>
+        <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-2">
+          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
+            <dt className="text-sm font-medium text-gray-500 truncate text-center">
+              Current Latitude
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">
+              {currentLat}
+            </dd>
+          </div>
+          <div className="bg-white shadow rounded-lg overflow-hidden sm:p-6">
+            <dt className="text-sm font-medium text-gray-500 truncate text-center">
+              Current Latitude
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold text-gray-900 text-center">
+              {currentLong}
+            </dd>
+          </div>
+        </dl>
         <button
-              type="button"
-              className="w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={generateProof}
-          >
-          Generate Proof
+          type="button"
+          className="w-full mt-5 items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          onClick={getLocation}
+        >
+          Get Location
         </button>
-        <button
-              type="button"
-              className="w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={verifyProof}
+        <div className="mt-5 grid grid-cols-2 gap-1 sm:grid-cols-2">
+          <button
+            type="button"
+            className="w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={generateProof}
           >
-          Verify Proof
-        </button>
+            Generate Proof
+          </button>
+          <button
+            type="button"
+            className="w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={verifyProof}
+          >
+            Verify Proof
+          </button>
+        </div>
+        {isVerified ? (
+          <button
+            type="button"
+            className="w-full mt-5 text-center items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={sendProps}
+          >
+            Next
+          </button>
+        ) : null}
       </div>
-      {
-        isVerified ? 
-                    <button
-                        type="button"
-                        className="w-full mt-5 text-center items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        onClick={sendProps}
-                    >
-                    Next
-                    </button> : null     
-            
-    }
-  </div>
-
-  
- 
-
-
-  </div>)
+    </div>
+  );
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MyComponent);
--- pages/google-map.tsx
+++ pages/google-map.tsx
@@ -1,13 +1,8 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-
-
+import { center } from "../components/mainPage";
 
 export default function googleMap() {
-
-
-
-
   return (
     <GoogleMap zoom={10} center={center}>
       <Marker position={center} />
