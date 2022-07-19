5f63635d6d14cb89c5fd93310616c79b28980076
danilowhk <danilowhk@gmail.com>
2022-07-19 05:40:42 -0300

    locationList

---
+++ components/data/group1.js
@@ -0,0 +1,36 @@
+export const group1Data = [
+  {
+    id:1,
+    group: 'NFT 1',
+    date: '14/02/2022',
+    latitude: '15.14',
+    longitude: '2.15',
+    country: 'Paris',
+  },
+  {
+    id:2,
+    group: 'NFT 1',
+    date: '14/02/2022',
+    latitude: '15.14',
+    longitude: '2.15',
+    country: 'Paris',
+  },
+  {
+    id:3,
+    group: 'NFT 1',
+    date: '14/02/2022',
+    latitude: '15.14',
+    longitude: '2.15',
+    country: 'Paris',
+  },
+  {
+    id:4,
+    group: 'NFT 1',
+    date: '14/02/2022',
+    latitude: '15.14',
+    longitude: '2.15',
+    country: 'Paris',
+  },
+
+
+]
--- components/locationList.tsx
+++ components/locationList.tsx
@@ -1,8 +1,8 @@
 /* This example requires Tailwind CSS v2.0+ */
-import {peopleData} from "./data/locations"
+import {group1Data} from "./data/group1"
   
   export const LocationList=() => {
-    const people = peopleData
+    const group1 = group1Data
     return (
       <div className="pl-60 pr-60 pt-10">
       <div className="px-4 sm:px-6 lg:px-8">
@@ -44,29 +44,29 @@ import {peopleData} from "./data/locations"
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 bg-white">
-                    {people.map((person) => (
-                      <tr key={person.email}>
+                    {group1.map((person) => (
+                      <tr key={person.id}>
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                           <div className="flex items-center">
-                            <div className="h-10 w-10 flex-shrink-0">
-                              <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                            </div>
+                        
                             <div className="ml-4">
-                              <div className="font-medium text-gray-900">{person.name}</div>
-                              <div className="text-gray-500">{person.email}</div>
+                              <div className="font-medium text-gray-900">{person.group}</div>
                             </div>
                           </div>
                         </td>
                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          <div className="text-gray-900">{person.title}</div>
-                          <div className="text-gray-500">{person.department}</div>
+                          <div className="text-gray-900">{person.date}</div>
+
                         </td>
                         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                            Active
-                          </span>
+                          <div className="text-gray-900">{person.latitude}</div>
                         </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.role}</td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <div className="text-gray-900">{person.longitude}</div>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <div className="text-gray-900">{person.country}</div>
+                        </td>
                         <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                           <a href="#" className="text-indigo-600 hover:text-indigo-900">
                             Message<span className="sr-only">, {person.name}</span>
@@ -105,13 +105,11 @@ import {peopleData} from "./data/locations"
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 bg-white">
-                    {people.map((person) => (
-                      <tr key={person.email}>
+                    {group1.map((person) => (
+                      <tr key={person.id}>
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                           <div className="flex items-center">
-                            <div className="h-10 w-10 flex-shrink-0">
-                              <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                            </div>
+                       
                             <div className="ml-4">
                               <div className="font-medium text-gray-900">{person.name}</div>
                               <div className="text-gray-500">{person.email}</div>
@@ -166,8 +164,8 @@ import {peopleData} from "./data/locations"
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 bg-white">
-                    {people.map((person) => (
-                      <tr key={person.email}>
+                    {group1.map((person) => (
+                      <tr key={person.id}>
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                           <div className="flex items-center">
                             <div className="h-10 w-10 flex-shrink-0">
